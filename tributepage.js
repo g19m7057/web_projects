8641269b59878e5d75cf5bf2ff93123e46ceb130
Sifisokuhle Mazibuko <Sifiokuhle>
2022-01-17 13:05:41 +0200

    include the tribute info in js file

--- tributepage.js
+++ tributepage.js
@@ -1,4 +1,8 @@
+const para = document.createElement("p");
+const info = document.createTextNode("Dr. Norman Borlaug was born in March 25, 1914, he was a agronomist who led what was known as the green revolution where there were increase in agricultural output, he was awarded multiple honours for he contributions including a noble peace prize the Presidential Medal of Freedom and the Congressional Gold Medal");
+para.appendChild(info);
+
 function fun()
 {
-    document.getElementById("tribute").innerHTML = "This is the tribute information";
+    document.getElementById("tribute-info").appendChild(para);
 }
