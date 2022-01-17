69f487e402b0af2e4d268320997a7d5cf82f14fe
Sifisokuhle Mazibuko <Sifiokuhle>
2022-01-17 21:42:09 +0200

    changed tributepage.js

--- tributepage.js
+++ tributepage.js
@@ -5,8 +5,8 @@ heading.appendChild(info);
 
 // list of tribute information
 const one = document.createTextNode("Dr. Norman Borlaug was born in March 25, 1914.")
-const two =  document.createTextNode("he was a agronomist who led what was known as the green revolution where there was an increase in agricultural output")
-const three = document.createTextNode("he was awarded multiple honours for he contributions including a noble peace prize the Presidential Medal of Freedom and the Congressional Gold Medal");
+const two =  document.createTextNode("He was a agronomist who led what was known as the green revolution where there was an increase in agricultural output")
+const three = document.createTextNode("He was also awarded multiple honours for he contributions including a noble peace prize the Presidential Medal of Freedom and the Congressional Gold Medal");
 
 const ul = document.createElement("ul");
 
@@ -21,12 +21,14 @@ ul.appendChild(li1);
 ul.appendChild(li2);
 ul.appendChild(li3);
 
-
 function fun()
 {
-    document.getElementById("tribute-info").appendChild(heading);
+    var div = document.getElementById("tribute-div");
+    div.style.marginTop = "40px";
+    document.getElementById("tribute-div").appendChild(heading);
     heading.style.textAlign = "center";
-    document.getElementById("tribute-info").appendChild(ul);
+    document.getElementById("tribute-div").appendChild(ul);
     ul.style.textAlign = "center";
     ul.style.listStyleType = "none";
+    document.getElementById("button").remove();
 }
